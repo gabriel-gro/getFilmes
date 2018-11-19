5fd929654d5db10b4a0c66fa4cd83385848b0c8d
gabriel-gro <gabriel.ribolive@gmail.com>
2018-11-19 13:25:07 -0200

    correcao de impressao

--- getMovies.py
+++ getMovies.py
@@ -1,41 +1,50 @@
 import requests
 import operator
 
-chave = '5b5be94f'
-titulo = input("Buscar filme por titulo! Digite o titulo: ")
-pagina = 1 # primeira pagina
 
-parametros = {'s': titulo, 'apikey': chave, 'page': pagina, 'type': 'movie'}
-r = requests.get('http://www.omdbapi.com/', params=parametros)
+def main():
+    chave = '5b5be94f'
+    titulo = input("Buscar filme por titulo! Digite o titulo: ")
+    pagina = 1 # primeira pagina
 
-data = r.json()
+    parametros = {'s': titulo, 'apikey': chave, 'page': pagina, 'type': 'movie'}
+    r = requests.get('http://www.omdbapi.com/', params=parametros)
 
-if(data['Response'] == 'False'):
-    print('Nenhum resultado encontrado')
-else:
-    nResultados = int(data['totalResults'])
-    lerResultados = len(data['Search'])
-    data['Search'] = sorted(iter(data['Search']), key=operator.itemgetter('Year'), reverse=True)
+    data = r.json()
 
-    for filme in data['Search']:
-        print(filme['Title'] + '\n' + filme['Year'] + '\n')
+    print("-------------------------------------------------------")
+    if(data['Response'] == 'False'):
+        print('Nenhum resultado encontrado')
+    else:
+        nResultados = int(data['totalResults'])
+        lerResultados = len(data['Search'])
+        data['Search'] = sorted(iter(data['Search']), key=operator.itemgetter('Year'), reverse=True)
 
-    sair = False
-    while(nResultados > lerResultados and sair == False):
-        op = input('Ver proxima pagina? [s|S para continuar]: ')
-        if(op == 's' or op == 'S'):
-            pagina += 1
-            parametros = {'s': titulo, 'apikey': chave, 'page': pagina, 'type': 'movie'}
-            r = requests.get('http://www.omdbapi.com/', params=parametros)
+        for filme in data['Search']:
+            print("\nFILME: " + filme['Title'] + '\n' + "ANO: " + filme['Year'] + '\n')
+            print("-------------------------------------------------------")
 
-            data = r.json()
-            data['Search'] = sorted(iter(data['Search']), key=operator.itemgetter('Year'), reverse=True)
+        sair = False
+        while(nResultados > lerResultados and sair == False):
+            op = input('Ver proxima pagina? [s|S para continuar]: ')
+            if(op == 's' or op == 'S'):
+                pagina += 1
+                parametros = {'s': titulo, 'apikey': chave, 'page': pagina, 'type': 'movie'}
+                r = requests.get('http://www.omdbapi.com/', params=parametros)
 
-            lerResultados += len(data['Search'])
+                data = r.json()
+                data['Search'] = sorted(iter(data['Search']), key=operator.itemgetter('Year'), reverse=True)
 
-            for filme in data['Search']:
-                print(filme['Title'] + '\n' + filme['Year'] + '\n')
+                lerResultados += len(data['Search'])
 
-        else:
-            sair = True
-print("Fim da listagem de filmes!!!")
+                for filme in data['Search']:
+                    print("\nFILME: " + filme['Title'] + '\n' + "ANO: " + filme['Year'] + '\n')
+                    print("-------------------------------------------------------")
+
+            else:
+                sair = True
+
+    print("Fim da listagem de filmes!!!")
+    print("-------------------------------------------------------")
+
+main()
